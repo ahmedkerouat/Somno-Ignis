8eccd52ce6b527b5fecd7d20b52bc27054ed78a1
Ahmed <105589873+ahmedkerouat@users.noreply.github.com>
2022-08-02 17:02:02 +0100

    created boundaries and updated the energy bar

--- entity.py
+++ entity.py
@@ -6,10 +6,92 @@ class SpriteSheet():
         self.sheet = img
 
     def get_image(self, frame, animation, width, height, scale, colour):
+        '''
+        Method Used to extract a portion of a spritesheet.
+        The method also scales up/down the sprite.
+        We can then simply use the sprite for our need.
+        '''
+
         img = py.Surface((width, height))
         img.blit(self.sheet, (0, 0), ((frame * width),
                  (width * animation), width, height))
         img = py.transform.scale(img, (width * scale, height * scale))
-        img.set_colorkey(colour)
+        img.set_colorkey(colour)  # for the transparency.
 
         return img
+
+
+class Player(py.sprite.Sprite):
+
+    def __init__(self, x, y, scale, speed):
+
+        self.x = x
+        self.y = y
+        self.height = 48
+        self.width = 50
+        self.scale = scale
+        self.speed = speed
+        self.flip = False
+        self.idle = True
+        self.run = False
+        self.attack = False
+        self.energy = 100
+        self.frame_index = 0
+
+        py.sprite.Sprite.__init__(self)
+
+        sprite_sheet_image_player = py.image.load(
+            'ressources\sprites\characters\player_fire.png').convert_alpha()
+        self.sprite_sheet_player = SpriteSheet(sprite_sheet_image_player)
+        self.img_player = self.sprite_sheet_player.get_image(
+            0, 0, self.height, self.width, 3, (0, 0, 0), )
+
+        self.image = py.transform.scale(
+            self.img_player, (int(self.img_player.get_width() * self.scale), int(self.img_player.get_height() * self.scale)))
+
+        self.last_update = py.time.get_ticks()
+
+    def energy_status(self, surface, font):
+
+        py.draw.rect(surface, (80, 80, 200), py.Rect(
+            10, 770, (self.energy * 2), 18), 0, 3)
+        py.draw.rect(surface, (80, 80, 200), py.Rect(
+            10, 770, (100 * 2), 18), 2, 3)
+        energy_render = font.render(
+            f"Energy : {round(self.energy) } %", 1, (255, 255, 255))
+
+        surface.blit(energy_render,
+                     (60, 772))
+
+    def animate(self, animation, max_frame):
+
+        # Method used for creating animation, depending on the player input.
+
+        if py.time.get_ticks() - self.last_update >= 50:
+            img_player = self.sprite_sheet_player.get_image(
+                self.frame_index, animation, self.height, self.width, 3, (0, 0, 0))
+            self.image = py.transform.scale(
+                img_player, (int(img_player.get_width() * self.scale), int(img_player.get_height() * self.scale)))
+            self.frame_index += 1
+            self.last_update = py.time.get_ticks()
+
+        if self.frame_index > max_frame:
+            self.frame_index = 0
+
+    def draw(self, surface):
+
+        # Method used for displaying the Player on the screen.
+
+        if self.idle == True:
+            self.animate(0, 5)
+
+        if self.run == True:
+            self.animate(1, 5)
+
+        if self.attack == True:
+            self.animate(2, 3)
+
+        self.rect = self.image.get_rect()
+        self.rect.center = (self.x, self.y)
+        surface.blit(py.transform.flip(
+            self.image, self.flip, False), self.rect)
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import pygame as py
 import sys
-from entity import SpriteSheet
+from entity import Player
 
 
 # constants
@@ -26,72 +26,7 @@ main_font = py.font.Font(
 # Player
 
 
-class Player(py.sprite.Sprite):
-
-    def __init__(self, x, y, scale, speed):
-
-        self.x = x
-        self.y = y
-        self.scale = scale
-        self.speed = speed
-        self.flip = False
-        self.idle = True
-        self.run = False
-        self.attack = False
-        self.energy = 100
-        self.frame_index = 0
-
-        py.sprite.Sprite.__init__(self)
-
-        sprite_sheet_image_player = py.image.load(
-            'ressources\sprites\characters\player_fire.png').convert_alpha()
-        self.sprite_sheet_player = SpriteSheet(sprite_sheet_image_player)
-        self.img_player = self.sprite_sheet_player.get_image(
-            0, 0, 50, 50, 3, (0, 0, 0), )
-
-        self.image = py.transform.scale(
-            self.img_player, (int(self.img_player.get_width() * self.scale), int(self.img_player.get_height() * self.scale)))
-
-        self.last_update = py.time.get_ticks()
-
-    def energy_status(self):
-        py.draw.rect(window, (80, 80, 200), py.Rect(
-            10, 770, (self.energy * 2), 18), 1, 3)
-        energy_render = main_font.render(
-            f"Energy : {round(self.energy) } %", 1, (255, 255, 255))
-        window.blit(energy_render,
-                    ((self.energy // 2), 772))
-
-    def animate(self, animation, max_frame):
-        if py.time.get_ticks() - self.last_update >= 50:
-            img_player = self.sprite_sheet_player.get_image(
-                self.frame_index, animation, 48, 50, 3, (0, 0, 0))
-            self.image = py.transform.scale(
-                img_player, (int(img_player.get_width() * self.scale), int(img_player.get_height() * self.scale)))
-            self.frame_index += 1
-            self.last_update = py.time.get_ticks()
-
-        if self.frame_index > max_frame:
-            self.frame_index = 0
-
-    def draw(self):
-
-        if self.idle == True:
-            self.animate(0, 5)
-
-        if self.run == True:
-            self.animate(1, 5)
-
-        if self.attack == True:
-            self.animate(2, 3)
-
-        self.rect = self.image.get_rect()
-        self.rect.center = (self.x, self.y)
-        window.blit(py.transform.flip(
-            self.image, self.flip, False), self.rect)
-
-
-player1 = Player(400, 400, 1, 2.5)
+player1 = Player(400, 400, 1, 3)
 
 # Controls
 
@@ -100,9 +35,13 @@ def game_input():
     keys = py.key.get_pressed()
     player1.idle = True
     player1.run = False
+    player1.attack == False
+
     if player1.idle == True:
         if player1.energy < 100:
             player1.energy += 0.1
+        if 50 < player1.energy < 99:
+            player1.energy *= 1.005
 
     if player1.energy > 10:
         if keys[py.K_SPACE]:
@@ -110,27 +49,27 @@ def game_input():
             player1.attack = True
             player1.energy -= 1
 
-        if keys[py.K_d]:
+        if keys[py.K_d] and player1.x < WIDTH - player1.width // 3:
             player1.flip = False
             player1.x += 1 * player1.speed
             player1.idle = False
             player1.run = True
             player1.energy -= 0.08
 
-        if keys[py.K_q]:
+        if keys[py.K_q] and player1.x > player1.width // 3:
             player1.flip = True
             player1.x -= 1 * player1.speed
             player1.idle = False
             player1.run = True
             player1.energy -= 0.08
 
-        if keys[py.K_s]:
+        if keys[py.K_s] and player1.y < HEIGHT - player1.height:
             player1.y += 1 * player1.speed
             player1.idle = False
             player1.run = True
             player1.energy -= 0.08
 
-        if keys[py.K_z]:
+        if keys[py.K_z] and player1.y > player1.height // 3:
             player1.y -= 1 * player1.speed
             player1.idle = False
             player1.run = True
@@ -145,8 +84,8 @@ def game_render():
     # window.blit(bg, (0,0))
     window.fill((50, 50, 50))
     points_render = main_font.render(f"Points : {points}", 1, WHITE)
-    player1.energy_status()
-    player1.draw()
+    player1.draw(window)
+    player1.energy_status(window, main_font)
     window.blit(points_render, (10, 10))
     # other things to blit.
     py.display.update()
@@ -167,4 +106,5 @@ def main():
         game_render()
 
 
-main()
+if __name__ == "__main__":
+    main()
